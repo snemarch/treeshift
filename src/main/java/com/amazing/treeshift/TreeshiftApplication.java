9bf4c6f3678bb44c24b27b4af4c13521bf802bd4
Sune Marcher <sm@flork.dk>
2019-11-05 21:04:59 +0100

    Add initial program skeleton
    Docker-compose file for the postgres database, including data volume.
    Gradle build files + wrapper, Spring Boot main application class,
    application.properties with datasource definition.

---
+++ src/main/java/com/amazing/treeshift/TreeshiftApplication.java
@@ -0,0 +1,11 @@
+package com.amazing.treeshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeshiftApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TreeshiftApplication.class, args);
+	}
+}
